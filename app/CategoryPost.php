63d60256c0d9da6676908837da1a7d8898da7e1a
jklhdd <49911153+jklhdd@users.noreply.github.com>
2020-04-21 19:24:51 +0700

    update

--- app/CategoryPost.php
+++ app/CategoryPost.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class CategoryPost extends Model
 {
-    protected $table = "category_post";
+    protected $table = "post_category";
     protected $fillable = ["name"];
 
     public function posts()
