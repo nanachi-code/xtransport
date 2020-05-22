6591901916a1a076b21483ac4371875459911a9a
lythaiho <lythaiho.95.cscd@gmail.com>
2020-05-22 10:03:50 +0700

    update
    1

--- resources/views/main/contact.blade.php
+++ resources/views/main/contact.blade.php
@@ -44,7 +44,7 @@
                 <!-- ======================== -->
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.092938382334!2d105.7796333142453!3d21.028966993148757!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313454b3285df81f%3A0x97be82a66bbe646b!2sDetech%20Building!5e0!3m2!1svi!2s!4v1588577870608!5m2!1svi!2s"
-                    width="600" height="534" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false"
+                    width="600" height="513" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false"
                     tabindex="0"></iframe>
                 <!-- ======================= -->
                 <!-- end -->
