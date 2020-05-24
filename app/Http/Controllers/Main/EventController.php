14b2bd9696ce74f7150a5e1377675efd8f67c00d
jklhdd <49911153+jklhdd@users.noreply.github.com>
2020-05-24 17:02:52 +0700

    Update EventController.php

--- app/Http/Controllers/Main/EventController.php
+++ app/Http/Controllers/Main/EventController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Mail\RegisterEvent;
 use App\Mail\UnRegisterEvent;
+use Illuminate\Support\Facades\Mail;
 
 class EventController extends Controller
 {
